51c6f75230b1dbaec8b37eb4f4acbec4dd614469
Dusan Vorkapic <dusan.vorkapic@futurice.com>
2017-11-01 17:54:46 +0100

    It won't connect with connection string and also even connected like this it always uses my local username database???

--- dbHelper.js
+++ dbHelper.js
@@ -1,16 +1,17 @@
 var postgresDB = require('pg');
 var knex = require('knex')({
   client: 'pg',
-  connection: process.env.DATABASE_URL,
-  // connection: {
-  //   host : process.env.DB_HOST,
-  //   port: process.env.DB_PORT,
-  //   user : process.env.DB_USER,
-  //   password : process.env.DB_PASS,
-  //   database : process.env.DB_NAME
-  // },
+  // connection: process.env.DATABASE_URL,
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  },
   searchPath: 'knex,public',
   debug: true,
+  ssl: true,
 });
 
 module.exports = { insertUser,
