e35664f33343733f3503a2d1f1318ceb2ee09d9d
Dusan Vorkapic <dusan.vorkapic@futurice.com>
2017-12-03 14:47:20 +0100

    Remove unused obsolete code

--- santaController.js
+++ santaController.js
@@ -27,49 +27,6 @@ res.status(200).send({ message : 'Welcome to the beginning of GET response!', })
 }
 }
 
-// function addUser(req,res) {
-//   const payload = req.body;
-//     if (!payload.email || !payload.password) {
-//       const errorMessage = { message: "Register call with invalid parameters" };
-//       res.status(400).send(errorMessage);
-//       return;
-//     }
-
-//     if (!validator.isEmail(payload.email)) {
-//       res.status(400).send({ message: "Invalid email!" });
-//       return;
-//     }
-
-//     var passHash = hashTool.getHashForString(payload.password);
-//     var userToken = tokenTool.getTokenForUserEmail(payload.email);
-
-//     dbHelper
-//       .insertUser(payload.name, payload.email, passHash)
-//       .then(() => {
-//         return dbHelper
-//           .getAllActiveOrganisations();
-//       })
-//       .then((organisations) => {
-//         res.status(200).send({ token: userToken, organisations: organisations });
-//         return;
-//       })
-//       .catch(res.status(500));
-// }
-
-// Temporary method, just for testing
-// function insertUser(req,res) {
-//   const payload = req.body;
-//   console.log("BLAAAAAAAAAAAAAAAA:", payload);
-//   dbHelper.insertUser(payload.name, payload.email, payload.firstName, payload.lastName, payload.accessToken, payload.serverAuthCode)
-//     .then((result) => {
-//       console.log("Rezultat:", result);
-//       res.status(200).send({ user_id: result });
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(500);
-//     });
-// }
 
 function setUser(req,res) {
   const payload = req.body;
@@ -120,37 +77,6 @@ function setUser(req,res) {
       });
 }
 
-// function getOrganisationInfo(req, res) {
-//   const payload = req.body;
-//   if (!payload.organisation_id || !payload.token) {
-//     const errorMessage = { message: "Register call with invalid parameters" };
-//     res.status(400).send(errorMessage);
-//     return;
-//   }
-
-// }
-
-// function verifyToken(req, res) {
-//   const payload = req.body;
-//   if (!payload.token) {
-//     const errorMessage = { message: "Register call with invalid parameters" };
-//     res.status(400).send(errorMessage);
-//     return;
-//   }
-
-//   tokenTool.validateToken(payload.token, (err, decoded) => {
-//     if (err) {
-//       console.error(err);
-//       res.status(400).send({ tokenValid: false });
-//       return;
-//     }
-//     if (decoded) {
-//       res.status(200).send({ tokenValid: true });
-//       return;
-//     }
-//   });
-// }
-
 function leaveOrganisation(req, res) {
   const payload = req.body;
   if (!payload.email ||
@@ -186,53 +112,6 @@ function leaveOrganisation(req, res) {
       res.status(500);
       return;
     });
-
-  // OLD CODE - where I had my own user token, not google one
-  // tokenTool.validateToken(payload.token, (err, decoded) => {
-  //   if (err) {
-  //     console.error(err);
-  //     res.status(400).send({ tokenValid: false });
-  //   }
-  //   if (decoded) {
-  //     var email = decoded;
-  //     var user_id;
-  //     dbHelper.getUserIdForEmail(email)
-  //     .then((userId) => {
-  //       user_id = userId;
-  //       return dbHelper.getOrganisationDetails(payload.organisation_id);
-  //     })
-  //     .then((organisation) => {
-  //       var deadlineDate = new Date(organisation.deadline);
-  //       var today = new Date();
-  //       if(today > deadlineDate) {
-  //         return;
-  //       } else {
-  //         return dbHelper.leaveOrganisation(payload.user_id, payload.organisation_id);
-  //       }
-  //     })
-  //     .then(() => {
-  //       return dbHelper.getAllActiveOrganisations();
-  //     })
-  //     .then((organisations) => {
-  //       res.status(200).send({ "organisations": organisations });
-  //       return;
-  //     })
-  //     .catch((err) => console.error(err));
-  //   }
-  // });
-}
-
-function listOfOrganisations(req, res) {
-  const payload = req.body;
-
-}
-
-function registerNewUser(req, res) {
-
-}
-
-function assignUserToOrganisation(req, res) {
-
 }
 
 function createOrganisationAddUserToIt(req, res) {
@@ -301,7 +180,6 @@ function joinOrganisation(req, res) {
       return dbHelper.joinOrganisation(user_id, payload.organisation_id)
     })
     .then((result) => {
-      // console.log("Result of joining organisation:", result)
       if (result === -1) {
         res.status(500);
         return -1
@@ -309,7 +187,6 @@ function joinOrganisation(req, res) {
       return dbHelper.getOrganisationDetails(payload.organisation_id);
     })
     .then((org_details) => {
-      // console.log("getOrganisationDetails result:", org_details);
       res.status(200).send(org_details);
     })
     .catch((err) => {
@@ -317,62 +194,4 @@ function joinOrganisation(req, res) {
       res.status(500);
       return;
     });
-
-  // dbHelper.joinOrganisation(payload.user_id, payload.organisation_id)
-  //   .then((result) => {
-  //     console.log("RES:", result);
-  //     res.status(200).send(result);
-  //     return;
-  //   })
-  //   .catch((err) => {
-  //     console.error(err);
-  //     res.status(500);
-  //     return;
-  //   });
 }
-
-// function loginUser(req, res) {
-//   const payload = req.body;
-//   if (!payload.email || !payload.password) {
-//     const errorMessage = { message: "Register call with invalid parameters" };
-//     res.status(400).send(JSON.stringify(errorMessage));
-//     return;
-//   }
-
-//   dbHelper
-//     .getUserRecord(payload.email)
-//     .then((row) => {
-//       var passCorrect = hashTool.verifyStringWithHash(payload.password, row.password);
-//       if (!passCorrect) {
-//         res.status(400).send({ error: "Incorrect password!" });
-//         return;
-//       }
-//       return dbHelper.isUserPartOfOrganisation(row.id); // check if part of Organisation
-//       }
-//     )
-//     .then((membershipRows) => {
-//       if(membershipRows.length > 0) {
-//         // YES: then return details of the organisation and token
-//         var userToken = tokenTool.getTokenForUserEmail(payload.email);
-//         dbHelper.getOrganisationDetails(membershipRows[0].org_id)
-//               .then((organisationDetails) => {
-//                 res.status(200).send({ "token": userToken, "organisation": organisationDetails })
-//                 return;
-//               })
-//               .catch((err) => console.error(err));
-//       } else {
-//         // NO: return list of organisations and token
-//         var userToken = tokenTool.getTokenForUserEmail(payload.email);
-//         dbHelper.getAllActiveOrganisations()
-//               .then((organisations) => {
-//                 res.status(200).send({ "token": userToken, "organisations": organisations });
-//                 return;
-//               })
-//               .catch((err) => console.error(err));
-//       }
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(500);
-//     });
-// }
