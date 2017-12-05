f24a1b8a0681e6783b9a9268fc8ebc4c927cb895
Dusan Vorkapic <dusan.vorkapic@futurice.com>
2017-12-05 09:51:53 +0100

    CORS

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ var app = express();
 //Enabling CORS - REMOVE THIS IN PRODUCTION OR SET TO CERTAIN DOMAINS!
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-PINGOTHER, Content-Type, Authorization, Content-Length, X-Requested-With");
+  re.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   next();
 });
 
